5d295e25e9f27efc4c5cd2ee4ee17e540ad01ad0
muhammadrizo <mt936425155@gmail.com>
2024-11-09 01:28:27 +0500

    Qo'shimcha

---
+++ main.js
@@ -0,0 +1,31 @@
+// 1-chitopshiriq
+// for(let i=0; i<10; i++) {
+//     console.log(i)
+// }
+// 2-chi topshiriq
+// for(let i=0; i<=20; i++) {
+//     if(i % 2 == 0) {
+//         console.log(i)
+//     }
+// }
+// 3-chi topshiriq
+// let b=0;
+// for(let i = 0; i<=100; i++) {
+//     b += i;
+// }
+// console.log(b)
+// 4-chi topshiriq
+// const numbers = [5, 6, 7, 8];
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+
+// console.log(sum);
+// 5-chi topshiriq
+// const users = ["Tohir", "Sobir", "Bakir"];
+
+// for (let i = 0; i < users.length; i++) {
+//     console.log(`salom ${users[i]}`);
+// }
